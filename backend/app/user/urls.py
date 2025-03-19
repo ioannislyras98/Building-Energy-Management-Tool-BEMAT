174db6789227c491d3670516d108b7b70a0676e4
ioannislyras98 <ioannis.lyras98@gmail.com>
2025-03-19 16:26:56 +0200

    19/3/2025

--- backend/app/user/urls.py
+++ backend/app/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import SignUpView
-from .views import ChangePasswordView
+from .views import SignUpView, ChangePasswordView, PasswordResetRequestView, PasswordResetConfirmView
 from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', obtain_auth_token, name='login'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
+    path('reset-password-confirm/<uid>/<token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 ]
